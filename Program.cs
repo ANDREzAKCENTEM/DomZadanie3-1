b60faaa2081b8fd54594eaf0706bee62741d84d0
Andy-Koo <DarikRunech@gmail.com>
2023-02-17 18:00:30 +0300

    Reshenie zadach

--- Program.cs
+++ Program.cs
@@ -1,27 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// // Архип Чижанов 14/02 20:02
-// // Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-// // 78 -> 8
-// // 12-> 2
-// // 85 -> 8
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// void MaxDecimal (int num)
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
 // {
-//     int ed = num % 10;
-//     int dec = num / 10;
-//     if(ed>dec)
-//     {
-//         Console.WriteLine($"Большая цифра числа {num} -> {ed}");        // return ed
-//     }
-//     else    
-//     {
-//         Console.WriteLine($"Большая цифра числа {num} -> {dec}");        // return dec
-//     }
+//     Console.WriteLine($"{num1} = max");
+// }
+// else
+// {
+//     Console.WriteLine($"{num2} = max");
 // }
 
-// int num = new Random().Next(10,99+1);
 
-// MaxDecimal (num);
 
-//15-46
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+
+// if (num1 > max)
+// {
+//     max = num1;
+// }
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+
+//  Console.WriteLine($"{max} = max");
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Четное или нечетное число. Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"Число {num} - четное");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num} - нечетное");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Поиск четных чисел до введенного числа. Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int n4 = 2;
+
+//  Console.Write($"Ряд четных чисел до числа {num}: ");
+
+// while (n4 < num)
+// {
+//          Console.Write($"  {n4} ");
+//     n4 = n4 + 2;
+// }
+
